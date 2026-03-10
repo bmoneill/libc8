38e4bb84ba2f7f62fadd92f4c0114e412bb098cf
Ben O'Neill <ben@oneill.sh>
2026-03-10 09:00:50 -0400

    Finish encode_decode test

--- test/test_encode_decode.c
+++ test/test_encode_decode.c
@@ -6,27 +6,35 @@
 
 #include "util.c"
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 extern C8_InstructionFormat c8_formats[];
 
-void                        setUp(void) {}
+void                        setUp(void) { srand(time(NULL)); }
 void                        tearDown(void) {}
 
 void                        test_encode_decode(void) {
     C8_InstructionFormat format;
-    uint8_t              gen_bytecode[2];
-    char                 gen_asm[16];
-    uint8_t              input[2];
-    uint16_t             params[3];
+    uint8_t              bytecode[2];
+    char asm[32];
+    uint16_t input;
+    uint16_t output;
 
     for (int i = 0; c8_formats[i].cmd != C8_I_NULL; i++) {
         // Generate bytecode for each format
-        format   = c8_formats[i];
-        input[0] = (format.base >> 8) & 0xFF;
-        input[1] = format.base & 0xFF;
+        format = c8_formats[i];
+        input  = format.base;
         for (int j = 0; j < format.pcount; j++) {
-            // TODO Finish
+            input |= rand() & format.pmask[j];
         }
 
         // TODO decode and encode, assert output is the same
+        sprintf(asm, "%s", c8_decode_instruction(input, NULL));
+        c8_encode(asm, bytecode, 0);
+        printf("%s\n", asm);
+        output = bytecode[0] << 8 | bytecode[1];
+        TEST_ASSERT_EQUAL_UINT16(input, output);
     }
 }
