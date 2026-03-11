8b82da23ee5f2eace27b80d174d6e7672201e9ae
Ben O'Neill <ben@oneill.sh>
2026-03-10 23:26:41 -0400

    Add comprehensive encode/decode test

--- test/test_encode_decode.c
+++ test/test_encode_decode.c
@@ -7,34 +7,22 @@
 #include "util.c"
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
-extern C8_InstructionFormat c8_formats[];
+void setUp(void) {}
+void tearDown(void) {}
 
-void                        setUp(void) { srand(time(NULL)); }
-void                        tearDown(void) {}
-
-void                        test_encode_decode(void) {
-    C8_InstructionFormat format;
-    uint8_t              bytecode[2];
+void test_encode_decode(void) {
+    uint8_t bytecode[2];
     char asm[32];
-    uint16_t input;
     uint16_t output;
 
-    for (int i = 0; c8_formats[i].cmd != C8_I_NULL; i++) {
-        // Generate bytecode for each format
-        format = c8_formats[i];
-        input  = format.base;
-        for (int j = 0; j < format.pcount; j++) {
-            input |= rand() & format.pmask[j];
-        }
-
-        // Decode and encode, assert output is the same
-        sprintf(asm, "%s", c8_decode_instruction(input, NULL));
+    // Decode and encode, assert output is the same
+    for (int i = 0; i <= 0xFFFF; i++) {
+        bytecode[0] = 0;
+        bytecode[1] = 0;
+        sprintf(asm, "%s\n", c8_decode_instruction(i, NULL));
         c8_encode(asm, bytecode, 0);
-        printf("%s\n", asm);
         output = bytecode[0] << 8 | bytecode[1];
-        TEST_ASSERT_EQUAL_UINT16(input, output);
+        TEST_ASSERT_EQUAL_UINT16(i, output);
     }
 }
